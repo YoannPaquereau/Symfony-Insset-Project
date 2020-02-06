9a55eab10b3b40de30bc189144b47c32075047c8
Yoann Paquereau <yoann.paquereau@gmail.com>
2020-02-06 21:37:47 +0100

    list products homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,5 +19,18 @@ class HomeController extends AbstractController
      */
     public function home(Request $request) {
         return $this->render('homepage.html.twig');
+    }
+
+    public function getProduct() {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findBy([
+                'available' => true
+            ]);
+
+        return $this->render('products/product.html.twig', [
+            'products' => $product,
+            'url' => $this->getParameter('app.path.product_images')
+        ]);
     }
 }
