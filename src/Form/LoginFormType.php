53b08c5dd4d960b0ebaf154733362e1379c2d76c
Yoann Paquereau <yoann.paquereau@gmail.com>
2020-02-18 19:27:45 +0100

    build login form

---
+++ src/Form/LoginFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ])
+            ->add('_remember_me', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Se souvenir de moi',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => ['class' => 'btn btn-primary btn-lg btn-block']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+           'csrf_protection' => true,
+           'csrf_field_name' => '_csrf_token',
+            'csrf_token_id' => 'authenticate'
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+
+}
