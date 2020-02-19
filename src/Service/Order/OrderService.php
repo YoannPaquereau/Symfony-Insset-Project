7676c66d03941a7b304b0538003946852af437c0
Yoann Paquereau <yoann.paquereau@gmail.com>
2020-02-19 13:55:55 +0100

    send email

--- src/Service/Order/OrderService.php
+++ src/Service/Order/OrderService.php
@@ -15,11 +15,13 @@ class OrderService
     protected $em;
     protected $basketService;
     protected $user;
+    protected $templating;
 
-    public function __construct(EntityManagerInterface $em, BasketService $basketService)
+    public function __construct(EntityManagerInterface $em, BasketService $basketService, \Twig\Environment $templating)
     {
         $this->em = $em;
         $this->basketService = $basketService;
+        $this->templating = $templating;
     }
     public function createOrder($userId) {
 
@@ -54,11 +56,30 @@ class OrderService
         $this->basketService->setConfirmOrder();
     }
 
-    public function  shippedOrder($id) {
+    public function shippedOrder($id, \Swift_Mailer $mailer) {
         $repository = $this->em->getRepository(Order::class);
 
         $entity = $repository->find($id);
         $entity->setShippingStatus(true);
         $this->em->flush();
+        $this->sendMail($entity, $mailer);
+    }
+
+    public function sendMail(Order $entity, \Swift_Mailer $mailer) {
+        $user = $entity->getIdUser();
+        $message = (new \Swift_Message('Avis d\'expédition de votre commande'))
+            ->setFrom([$_ENV['MAILER_EMAIL'] => 'Yoann Paquereau'])
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->templating->render('order/mail.txt.twig', [
+                    'first_name' => $user->getFirstName(),
+                    'last_name' => $user->getLastName()
+                ]),
+                'text/plain'
+            )
+        ;
+
+
+        $mailer->send($message);
     }
 }
